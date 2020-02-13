e309a5343695853aaf1a0e78088911711068b428
Luda-Glazova <lusy.90@mail.ru>
2020-02-13 20:55:57 +0400

    123
    123

--- Sandbox/src/main/java/Helloworld.java
+++ Sandbox/src/main/java/Helloworld.java
@@ -4,22 +4,14 @@ public class Helloworld {
 	hello("world");
 	hello("user");
 	hello("Luda");
+	Square s= new Square(5);
 
-	double l=5;
-		System.out.println("Площадь квадрата со стороной " + l + "= " + area(l) );
+		System.out.println("Площадь квадрата со стороной " + s.l + "= " + s.area());
+	Rectangle r= new Rectangle(4,6);
 
-		double a=4;
-		double b=6;
-		System.out.println("Площадь прямоуголльника со сторонами " + a + " и " + b + "= " + area(a,b) );
+		System.out.println("Площадь прямоуголльника со сторонами " + r.a + " и " + r.b + "= " + r.area());
 	}
 	public static void hello(String somebody){
 		System.out.println("Hello, " + somebody + "!");
-	}
-
-	public static double area (double len) {
-		return len*len;
-	}
-	public static double area (double a, double b) {
-		return a*b;
 	}
 }
