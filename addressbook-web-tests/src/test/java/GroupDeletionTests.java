c67209ff45643eaec4c9f65eb17eb50b46558838
Luda-Glazova <lusy.90@mail.ru>
2020-03-11 17:29:00 +0400

    наследование не миллионов((
    наследование не миллионов((

---
+++ addressbook-web-tests/src/test/java/GroupDeletionTests.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletionTests() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+}
